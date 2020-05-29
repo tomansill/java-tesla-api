f14aa94b12e1b374ca259320007c8cb889341d4f
Tom Ansill <tom@ansill.com>
2020-05-29 11:39:13 -0600

    - Removed global variable for data lifetimes - Added class field variable for data lifetimes

--- api/src/main/java/com/ansill/tesla/api/high/Client.java
+++ api/src/main/java/com/ansill/tesla/api/high/Client.java
@@ -7,6 +7,7 @@ import com.ansill.tesla.api.raw.exception.ReAuthenticationException;
 import com.ansill.validation.Validation;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.time.Duration;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
@@ -21,24 +22,35 @@ public final class Client{
   /** Default slow changing data lifetime - used to prevent too-frequent polling */
   private static final Duration DEFAULT_SLOW_CHANGING_DATA_LIFETIME = Duration.ofMinutes(1);
 
-  /** Global setting */
-  private static final AtomicReference<Duration> GLOBAL_FAST_CHANGING_DATA_LIFETIME = new AtomicReference<>(
-    DEFAULT_FAST_CHANGING_DATA_LIFETIME);
-
-  /** Global setting */
-  private static final AtomicReference<Duration> GLOBAL_SLOW_CHANGING_DATA_LIFETIME = new AtomicReference<>(
-    DEFAULT_SLOW_CHANGING_DATA_LIFETIME);
-
   /** Low-level client */
+  @Nonnull
   private final com.ansill.tesla.api.low.Client client;
+
+  /** Lifetime in duration for fast-changing data to be cached in the memory before it's purged */
+  @Nonnull
+  private final AtomicReference<Duration> fastChangingDataLifetime = new AtomicReference<>();
+
+  /** Lifetime in duration for slow-changing data to be cached in the memory before it's purged */
+  @Nonnull
+  private final AtomicReference<Duration> slowChangingDataLifetime = new AtomicReference<>();
 
   /**
    * Constructor that constructs high-level client using medium-level client
    *
-   * @param client medium-level client
+   * @param client                   low-level client
+   * @param fastChangingDataLifetime Lifetime in duration for fast-changing data to be cached in the memory before it's purged
+   * @param slowChangingDataLifetime Lifetime in duration for slow-changing data to be cached in the memory before it's purged
    */
-  private Client(@Nonnull com.ansill.tesla.api.low.Client client){
+  private Client(
+    @Nonnull com.ansill.tesla.api.low.Client client,
+    @Nullable Duration fastChangingDataLifetime,
+    @Nullable Duration slowChangingDataLifetime
+  ){
     this.client = client;
+    this.fastChangingDataLifetime.set(fastChangingDataLifetime !=
+                                      null ? fastChangingDataLifetime : DEFAULT_FAST_CHANGING_DATA_LIFETIME);
+    this.slowChangingDataLifetime.set(slowChangingDataLifetime !=
+                                      null ? slowChangingDataLifetime : DEFAULT_SLOW_CHANGING_DATA_LIFETIME);
   }
 
   /**
@@ -51,35 +63,32 @@ public final class Client{
     return new com.ansill.tesla.api.high.Client.Builder();
   }
 
-  /** Builder */
-  public static class Builder extends ClientBuilder<com.ansill.tesla.api.high.Client>{
-
-    @Nonnull
-    @Override
-    public com.ansill.tesla.api.high.Client build(){
-      return new com.ansill.tesla.api.high.Client(com.ansill.tesla.api.low.Client.builder()
-                                                                                 .setUrl(this.url)
-                                                                                 .setClientId(clientId)
-                                                                                 .setClientSecret(clientSecret)
-                                                                                 .setDebugFunction(debug)
-                                                                                 .build());
-    }
+  /**
+   * Sets lifetime in duration for fast-changing data to be cached in the memory before it's purged
+   *
+   * @param duration lifetime in duration
+   */
+  public void setFastChangingDataLifetime(@Nonnull Duration duration){
+    fastChangingDataLifetime.set(Validation.assertNonnull(duration, "duration"));
   }
 
-  public void setGlobalFastChangingDataLifetime(@Nonnull Duration duration){
-    GLOBAL_FAST_CHANGING_DATA_LIFETIME.set(Validation.assertNonnull(duration, "duration"));
+  /**
+   * Sets lifetime in duration for slow-changing data to be cached in the memory before it's purged
+   *
+   * @param duration lifetime in duration
+   */
+  public void setSlowChangingDataLifetime(@Nonnull Duration duration){
+    slowChangingDataLifetime.set(Validation.assertNonnull(duration, "duration"));
   }
 
-  public void setGlobalSlowChangingDataLifetime(@Nonnull Duration duration){
-    GLOBAL_SLOW_CHANGING_DATA_LIFETIME.set(Validation.assertNonnull(duration, "duration"));
+  /** Resets lifetime in duration for fast-changing data to default */
+  public void resetFastChangingDataLifetime(){
+    fastChangingDataLifetime.set(DEFAULT_FAST_CHANGING_DATA_LIFETIME);
   }
 
-  public void resetGlobalFastChangingDataLifetime(){
-    GLOBAL_FAST_CHANGING_DATA_LIFETIME.set(DEFAULT_FAST_CHANGING_DATA_LIFETIME);
-  }
-
-  public void resetGlobalSlowChangingDataLifetime(){
-    GLOBAL_SLOW_CHANGING_DATA_LIFETIME.set(DEFAULT_SLOW_CHANGING_DATA_LIFETIME);
+  /** Resets lifetime in duration for slow-changing data to default */
+  public void resetSlowChangingDataLifetime(){
+    slowChangingDataLifetime.set(DEFAULT_SLOW_CHANGING_DATA_LIFETIME);
   }
 
   /**
@@ -95,8 +104,8 @@ public final class Client{
       return Optional.of(new Account(
         client,
         client.authenticate(emailAddress, password),
-        new AtomicReference<>(GLOBAL_FAST_CHANGING_DATA_LIFETIME),
-        new AtomicReference<>(GLOBAL_SLOW_CHANGING_DATA_LIFETIME)
+        new AtomicReference<>(fastChangingDataLifetime),
+        new AtomicReference<>(slowChangingDataLifetime)
       ));
     }catch(AuthenticationException e){
       return Optional.empty();
@@ -115,14 +124,13 @@ public final class Client{
       return Optional.of(new Account(
         client,
         client.refreshToken(refreshToken),
-        new AtomicReference<>(GLOBAL_FAST_CHANGING_DATA_LIFETIME),
-        new AtomicReference<>(GLOBAL_SLOW_CHANGING_DATA_LIFETIME)
+        new AtomicReference<>(fastChangingDataLifetime),
+        new AtomicReference<>(slowChangingDataLifetime)
       ));
     }catch(ReAuthenticationException e){
       return Optional.empty();
     }
   }
-
 
   /**
    * Authenticates to tesla account while returning credentials
@@ -148,8 +156,8 @@ public final class Client{
       return Optional.of(new Account(
         client,
         newCred,
-        new AtomicReference<>(GLOBAL_FAST_CHANGING_DATA_LIFETIME),
-        new AtomicReference<>(GLOBAL_SLOW_CHANGING_DATA_LIFETIME),
+        new AtomicReference<>(fastChangingDataLifetime),
+        new AtomicReference<>(slowChangingDataLifetime),
         new RefreshSubscription(
           consumer,
           Validation.assertNonnull(onError, "onError")
@@ -157,6 +165,52 @@ public final class Client{
       ));
     }catch(ReAuthenticationException e){
       return Optional.empty();
+    }
+  }
+
+  /** Builder */
+  public static class Builder extends ClientBuilder<com.ansill.tesla.api.high.Client>{
+
+    /** Lifetime in duration for fast-changing data to be cached in the memory before it's purged */
+    private Duration fastChangingDataLifetime;
+
+    /** Lifetime in duration for slow-changing data to be cached in the memory before it's purged */
+    private Duration slowChangingDataLifetime;
+
+    @Nonnull
+    @Override
+    public com.ansill.tesla.api.high.Client build(){
+      var client = com.ansill.tesla.api.low.Client.builder()
+                                                  .setUrl(this.url)
+                                                  .setClientId(clientId)
+                                                  .setClientSecret(clientSecret)
+                                                  .setDebugFunction(debug)
+                                                  .build();
+      return new com.ansill.tesla.api.high.Client(client, fastChangingDataLifetime, slowChangingDataLifetime);
+    }
+
+    /**
+     * Sets duration in fast-changing data
+     *
+     * @param fastChangingDataLifetime Lifetime in duration for fast-changing data to be cached in the memory before it's purged
+     * @return updated builder
+     */
+    @Nonnull
+    public Builder setFastChangingDataLifetime(Duration fastChangingDataLifetime){
+      this.fastChangingDataLifetime = fastChangingDataLifetime;
+      return this;
+    }
+
+    /**
+     * Sets duration in slow-changing data
+     *
+     * @param slowChangingDataLifetime Lifetime in duration for slow-changing data to be cached in the memory before it's purged
+     * @return updated builder
+     */
+    @Nonnull
+    public Builder setSlowChangingDataLifetime(Duration slowChangingDataLifetime){
+      this.slowChangingDataLifetime = slowChangingDataLifetime;
+      return this;
     }
   }
 }
